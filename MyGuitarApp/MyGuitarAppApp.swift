f8d8b08cc006961ed0b0e9f5ffcaed7d0fc36035
pjh347 <gownj333@gmail.com>
2025-11-26 15:11:37 +0900

    Initial Commit

---
+++ MyGuitarApp/MyGuitarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGuitarAppApp.swift
+//  MyGuitarApp
+//
+//  Created by 박준형 on 11/26/25.
+//
+
+import SwiftUI
+
+@main
+struct MyGuitarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyGuitarAppTests/MyGuitarAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyGuitarAppTests.swift
+//  MyGuitarAppTests
+//
+//  Created by 박준형 on 11/26/25.
+//
+
+import Testing
+@testable import MyGuitarApp
+
+struct MyGuitarAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
